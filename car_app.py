d82c21965c4e905dafba611d77012f9f0817729b
akshay-niranjan <akshayniranjan51@gmail.com>
2025-09-23 00:48:59 +0530

    Update car_app.py

--- car_app.py
+++ car_app.py
@@ -4,23 +4,27 @@ import pandas as pd
 import numpy as np
 
 # Load the model and data
-model = pickle.load(open('LinearRegressionModel.pkl','rb'))
-car = pd.read_csv('Cleaned Car.csv')
+model = pickle.load(open('Project_Car_Price_Predictor/LinearRegressionModel.pkl','rb'))
+car = pd.read_csv('Project_Car_Price_Predictor/Cleaned Car.csv')
 
 st.title("🚗 Car Price Predictor")
 
-# Dropdowns and inputs
+# Dropdown for company
 companies = sorted(car['company'].unique())
-car_models = sorted(car['name'].unique())
+company = st.selectbox("Select Company", companies)
+
+# Filter car models based on selected company
+filtered_models = car[car['company'] == company]['name'].unique()
+car_model = st.selectbox("Select Car Model", sorted(filtered_models))
+
+# Other inputs
 years = sorted(car['year'].unique(), reverse=True)
 fuel_types = car['fuel_type'].unique()
-
-company = st.selectbox("Select Company", companies)
-car_model = st.selectbox("Select Car Model", car_models)
 year = st.selectbox("Select Year", years)
 fuel_type = st.selectbox("Select Fuel Type", fuel_types)
 kms_driven = st.number_input("KMs Driven", min_value=0, max_value=500000, step=500)
 
+# Prediction
 if st.button("Predict Price"):
     input_data = pd.DataFrame(
         [[car_model, company, year, kms_driven, fuel_type]],
